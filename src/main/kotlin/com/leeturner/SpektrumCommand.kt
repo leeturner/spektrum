64817ae25601da3ea4e57e7984a29ed3f7aebc3a
leeturner <lee@leeturner.me>
2025-09-30 18:34:06 +0100

    Initial commit of spektrum emulator

---
+++ src/main/kotlin/com/leeturner/SpektrumCommand.kt
@@ -0,0 +1,30 @@
+package com.leeturner
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "spektrum", description = ["..."],
+        mixinStandardHelpOptions = true)
+class SpektrumCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(SpektrumCommand::class.java, *args)
+        }
+    }
+}
